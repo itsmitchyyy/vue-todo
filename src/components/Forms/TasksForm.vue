f0d22fb3b287e0e87e2ba9f1785c03174535e20d
Mitch Alforque <mitch@vananaz.com>
2022-07-13 13:52:41 +0800

    added project on task form

--- src/components/Forms/TasksForm.vue
+++ src/components/Forms/TasksForm.vue
@@ -12,6 +12,15 @@
     <label for="taskDescription" class="form-label">Task Description</label>
     <textarea class="form-control" id="taskDescription" rows="3"></textarea>
   </div>
+  <div class="mb-3">
+    <label for="taskDescription" class="form-label">Project</label>
+    <select class="form-select" aria-label="Default select example">
+      <option selected>Open this select menu</option>
+      <option value="1">One</option>
+      <option value="2">Two</option>
+      <option value="3">Three</option>
+    </select>
+  </div>
   <div>
     <button type="button" class="btn btn-primary w-100">Submit</button>
   </div>
